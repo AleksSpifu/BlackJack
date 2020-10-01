f3996fd0f152d69803afac1c04fdf2a67224be40
AleksSpifu <aleksander.s.furnes@gmail.com>
2020-10-01 21:30:04 +0200

    Commented everything

--- BlackJack/Source.cpp
+++ BlackJack/Source.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <conio.h>
-#include <Windows.h>
+#include <stdlib.h> //For random number.
+#include <time.h> //For seed for the random number.
+#include <stdio.h> //For NULL for the random number.
+#include <conio.h> //For _getch
+#include <Windows.h> //for Sleep
 
 void populateDeckOfCards(std::vector <int>& deck, std::vector <int>& drawn, int deckSize);
 void drawCard(std::vector <int>& deck, std::vector <int>& drawn, std::vector <int>& drawnValue);
@@ -16,28 +16,38 @@ int calculateValueOfHouseCards(std::vector <int>& drawn, std::vector <int>& draw
 
 
 int main() {
+
+	//Declare a lot of variables.
 	int moneyPot = 0;
 	int playerMoney = 100;
 	int houseMoney = 100;
-	int deckSize = 52;
+	const int deckSize = 52;
 	int lastScore = 0;
 	std::vector <int> drawnCards;
 	std::vector <int> houseDrawnCards;
 	std::vector <int> houseDrawnCardsValue;
 	std::vector <int> drawnCardsValue;
 	std::vector <int> deckOfCards;
-
 	bool gameover = false;
 	bool playerFinished = false;
+
+	//Run as long as there is money to win, either from house or in the pot, or until player runs out.
 	while (playerMoney > 0 && (houseMoney > 0 || moneyPot > 0)) {
+
+		//Make sure the game runs again after first win/loss
 		playerFinished = false;
 		gameover = false;
+
+		//Reset the deck so all the cards go back to their original place.
+		//Also reset the cards held by the house and player, as well as the values for the Aces.
 		populateDeckOfCards(deckOfCards, drawnCards, deckSize);
 		drawnCards.clear();
 		houseDrawnCards.clear();
 		houseDrawnCardsValue.clear();
 		drawnCardsValue.clear();
 		lastScore = 0;
+
+		//This is the main game.
 		while (!gameover) {
 			system("cls");
 			std::cout << "Welcome to BlackJack! \n\nPress D to draw a new card, S to stop, or Escape to exit.\n";
@@ -46,6 +56,9 @@ int main() {
 			if (drawnCards.size() == 0) {
 				std::cout << "There is a $10 fee for starting a game.\n";
 			}
+
+			//Display all the cards held by the player. If there are none, dislay none.
+			//Also display the value of all the cards combined. 
 			std::cout << "\nYou are currently holding ";
 			if (drawnCards.size() > 0) {
 				for (int i = 0; i < drawnCards.size(); i++) {
@@ -57,6 +70,9 @@ int main() {
 						std::cout << ", ";
 					}
 				}
+
+				//Calculate function will ask for clarification on wether or not Ace should be 1 or 11.
+				//Then print the total value.
 				lastScore = calculateValueOfHeldCards(drawnCards, drawnCardsValue, lastScore);
 				std::cout << "With a combined value of " << lastScore << "\n";
 				
@@ -64,9 +80,15 @@ int main() {
 			else {
 				std::cout << "no cards.\n";
 			}
+
+			//Force the player to quit if they hit or exceed 21.
 			if (lastScore >= 21) {
 				playerFinished = true;
 			}
+
+			//This handles all the input for playing, stopping or exiting the program.
+			//It forces both while-loops to stop by setting all the money to 0.
+			//It also handles input for the betting system, which is expanded upon below.
 			if (!playerFinished) {
 				int betamount = 0;
 				switch (tolower(_getch())) {
@@ -116,6 +138,10 @@ int main() {
 				default:
 					break;
 				}
+
+				//Pretty self-explanatory. It just tries to put money in the pot, and if it can't, it'll just say can't afford.
+				//Then the inner while-loop will run through and pick up where it left off, allowing the user to play or bet less.
+				//there is nothing stopping the player from betting $10 several times, instead of one large sum.
 				if (betamount > 0) {
 					if (playerMoney >= (betamount * 10) && houseMoney >= (betamount * 10)) {
 						playerMoney -= (betamount * 10);
@@ -129,6 +155,11 @@ int main() {
 				}
 				
 			}
+
+			//This just figures out who won and if it's a tie. If it's a tie, the betting pot will remain.
+			//I don't know blackjack but i couldn't find out who it goes to. Also to be kind to the player, 
+			//rather than immediatly say they lost if they exceed 21, i let the AI try, and if it also fails, 
+			//it counts as a draw, and the money can still be won.
 			if (playerFinished) {
 				int houseScore = housePlays(deckOfCards, houseDrawnCards, houseDrawnCardsValue, lastScore);
 				if (houseScore > 21 && lastScore > 21) {
@@ -172,26 +203,37 @@ int main() {
 
 }
 
+//This function is the brains of the house. It will draw cards, and also display everything. 
+//It returns the value of all it's cards back to main for checking who won, but all the cout's are from here.
 int housePlays(std::vector <int>& deck, std::vector <int>& houseDrawn, std::vector <int>& houseDrawnValue, int playerScore) {
 	int counter = 0;
 	int houseScore = 0;
+	
+	//No matter what, it will keep drawing cards until it reaches 10.
 	while (houseScore < 10) {
 		drawCard(deck, houseDrawn, houseDrawnValue);
 		std::cout << "House draws";
+
+		//Add some suspensful waiting time.
 		for (int i = 0; i < 4; i++) {
 			Sleep(125);
 			std::cout << ".";
 			Sleep(125);
 		}
 
+		//Tell the player what card it got.
 		std::cout << " " << printCard(houseDrawn[counter]);
 		Sleep(1000);
 
+		//Then tell the player the value of the cards.
 		houseScore = calculateValueOfHouseCards(houseDrawn, houseDrawnValue, houseScore);
-
-		std::cout << " Score: " << houseScore << "\n";
+		std::cout << " Value: " << houseScore << "\n";
 		counter++;
 	}
+
+	//If the player is closer than the house, it has nothing to lose, and will keep going.
+	//If the house is closer to 21 than the player, it will happily stop after 10.
+	//Then do the same as above, draw card, tell the player the card and value.
 	while (houseScore < playerScore && playerScore <= 21) {
 		drawCard(deck, houseDrawn, houseDrawnValue);
 		std::cout << "House draws";
@@ -208,11 +250,20 @@ int housePlays(std::vector <int>& deck, std::vector <int>& houseDrawn, std::vect
 		std::cout << " Score: " << houseScore << "\n";
 		counter++;
 	}
+
+	//Then send the total value back for comparison with the players score.
 	return houseScore;
 }
 
+//This is just for printing the cards with fancy icons. Cards are stored as a 3 digit number, this just turns it into 
+//a more user-friendly text.
 std::string printCard(int cardInput) {
 	std::string output;
+
+	//See populateDeckOfCards for spesifics, but basically, every card has an ID of three digits, for example "203"
+	//This means it is a 3 card, of the type '\4'. Therefore, i insert the correct type into the output string, 
+	//and then remove the type-digit from the integer. This doesn't change the value of the original card, it's just for displaying.
+	//Then I add the number or Icon (K for King, Q for Queen, etc.) and return that as a string.
 	if (cardInput < 200 && output.size() == 0) {
 		output.push_back('\3');
 		cardInput = cardInput - 100;
@@ -277,6 +328,8 @@ std::string printCard(int cardInput) {
 	return output;
 }
 
+//This will get a random number, attempt to take that card from the deck, and if it's taken, it will take another.
+//if the player somehow draws 52 cards, it will stop. This is to prevent it from going on an infinite loop if something goes wrong.
 void drawCard(std::vector <int>& deck, std::vector <int>& drawn, std::vector <int>& drawnValue) {
 	srand(time(NULL));
 	bool selectionAccepted = false;
@@ -291,6 +344,9 @@ void drawCard(std::vector <int>& deck, std::vector <int>& drawn, std::vector <in
 	}
 }
 
+//This is where the deck gets reset. It will give every card it's unique ID of 3 digits, and put them all in the deck vector. 
+//The deck is in the same order as a normal deck, and doesn't get shuffled, however the cards chosen are randomized so i don't
+//think that will be an issue. 
 void populateDeckOfCards(std::vector <int>& deck, std::vector <int>& drawn, int deckSize) {
 	deck.clear();
 	drawn.clear();
@@ -310,6 +366,9 @@ void populateDeckOfCards(std::vector <int>& deck, std::vector <int>& drawn, int 
 	
 }
 
+//This will strip away the type-digit from a cards ID, and add it to the total value. If the card is an Ace,
+//The player will get the option to either select 1 or 11. It will be stored in drawnValue, so the player doesn't
+//have to answer every turn. the same goes for the house.
 int calculateValueOfHeldCards(std::vector <int>& drawn, std::vector <int>& drawnValue, int lastScore) {
 	int cardValue = 0;
 	int totalValue = 0;
@@ -317,16 +376,24 @@ int calculateValueOfHeldCards(std::vector <int>& drawn, std::vector <int>& drawn
 		for (int i = 0; i < drawn.size(); i++) {
 			cardValue = drawn[i];
 			int type = 0;
+
+			//remove the type-digit.
 			while (cardValue > 100) {
 				cardValue -= 100;
 				type++;
 			}
+
+			//if it's a regular number card, just add it's number to the totalvalue.
 			if (cardValue >= 2 && cardValue < 11) {
 				totalValue += cardValue;
 			}
+
+			//If it's a jack, queen or king, add 10.
 			else if (cardValue >= 11) {
 				totalValue += 10;
 			}
+
+			//if its an Ace, ask the player what they want to do.
 			else if (cardValue == 1 && drawnValue[i] == 0) {
 				std::cout << "So far you have " << lastScore << ". Would you like ";
 				std::cout << printCard(100 * type + cardValue);
@@ -351,6 +418,8 @@ int calculateValueOfHeldCards(std::vector <int>& drawn, std::vector <int>& drawn
 					}
 				}
 			}
+
+			//or if the player has already answered for that particular ace, just add whatever they chose to totalvalue.
 			else if (cardValue == 1 && drawnValue[i] != 0) {
 				totalValue += drawnValue[i];
 			}
@@ -362,6 +431,7 @@ int calculateValueOfHeldCards(std::vector <int>& drawn, std::vector <int>& drawn
 	}
 }
 
+//This pretty much does the same as for the player except it chooses automatically.
 int calculateValueOfHouseCards(std::vector <int>& drawn, std::vector <int>& drawnValue, int lastScore) {
 	int cardValue = 0;
 	int totalValue = 0;
@@ -380,6 +450,8 @@ int calculateValueOfHouseCards(std::vector <int>& drawn, std::vector <int>& draw
 				totalValue += 10;
 			}
 			else if (cardValue == 1 && drawnValue[i] == 0) {
+
+				//if it has 10 or less, it will take 11, but if it has more, it has to take 1.
 				if (lastScore <= 10) {
 					drawnValue[i] = 11;
 					totalValue += drawnValue[i];
